f86f49a1d10fc74e2cd285ae5f99960041293412
chelsigorridon <chelsijaimegorridon@gmail.com>
2026-04-02 19:14:25 +0200

    Adding files and HTML styling

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+theme: {
+    extend: {
+      fontFamily: {
+        tan: ['TanPearl', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        tan: ['"TAN-PEARL"', 'serif'],
+      },
+    },
+  },
+}
