460d654eab8c5d8e61ecbae544f9c610869675a5
chelsigorridon <chelsijaimegorridon@gmail.com>
2026-04-03 11:31:02 +0200

    imported service script

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// script.js
+import { initServicesDropdown } from './servicesdropdown.js';
+
+// Initialize dropdown when page loads
+document.addEventListener('DOMContentLoaded', () => {
+  initServicesDropdown();
+});
+
+
