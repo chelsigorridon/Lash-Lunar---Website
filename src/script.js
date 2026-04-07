e9c4cf8e3290e9e8af31a1195b269d26a381e786
chelsigorridon <chelsijaimegorridon@gmail.com>
2026-04-07 02:41:23 +0200

    updating all files

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
 // script.js
 import { initServicesDropdown } from './servicesdropdown.js';
+import { mobileNav } from './mobilenav.js'; 
 
 // Initialize dropdown when page loads
 document.addEventListener('DOMContentLoaded', () => {
   initServicesDropdown();
+  mobileNav();
 });
 
 
