1b5d50824c3cc17048cf644c1946fc22b8fe26ff
chelsigorridon <chelsijaimegorridon@gmail.com>
2026-04-07 23:33:03 +0200

    rendered content in DOM

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
 // script.js
 import { initServicesDropdown } from './servicesdropdown.js';
 import { mobileNav } from './mobilenav.js'; 
+import { initSmoothScroll } from './smoothscroll.js';
 
 // Initialize dropdown when page loads
 document.addEventListener('DOMContentLoaded', () => {
   initServicesDropdown();
   mobileNav();
+  initSmoothScroll();
 });
 
 
